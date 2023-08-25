eb2a7572d1dd7068118fc98f7fc58bd650f3a74e
KlimVladMaks <vkm.blog@yandex.ru>
2023-08-25 15:30:26 +0300

    Начало работы над корзиной для товаров

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import styles from './Header.module.css'
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Header = () => {
+
+    let [isCartOpen, setIsCartOpen] = useState(false);
+
     return (
-        <header>
-            <div>
+        <header className={styles.header}>
+            <div className={styles['header-top']}>
                 <span className={styles.logo}>Online Store</span>
-                <ul className={styles.nav}>
-                    <li>О нас</li>
-                    <li>Контакты</li>
-                    <li>Кабинет</li>
-                </ul>
+                <FaShoppingCart className={`${styles['shop-cart-button']} ${isCartOpen && styles.active}`}
+                                onClick={() => setIsCartOpen(!isCartOpen)} />
             </div>
-            <div className={styles.presentation}></div>
+            {isCartOpen && (
+                <div className={styles['shop-cart']}></div>
+                )}
+            <div className={styles.banner}></div>
         </header>
     );
 }
