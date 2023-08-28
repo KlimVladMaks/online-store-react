40570a6688995f91c300a37bf40b64cbfd5cd2c6
KlimVladMaks <vkm.blog@yandex.ru>
2023-08-28 23:50:05 +0300

    Добавлена кнопка для удаления товара из корзины

--- src/components/home/header/cart-list/cart-item/CartItem.jsx
+++ src/components/home/header/cart-list/cart-item/CartItem.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
 
 import styles from './CartItem.module.css';
+import { FaTrash } from 'react-icons/fa';
 
 const CartItem = ({order}) => {
     return (
         <div className={styles.item}>
-            <img className={styles.img} src={order.img} alt={order.title} />
-            <div>
-                <h2 className={styles.title}>{order.title}</h2>
-                <b className={styles.price}>{order.price} ₽</b>
+            <div className={styles['product-info']}>
+                <img className={styles.img} src={order.img} alt={order.title} />
+                <div>
+                    <h2 className={styles.title}>{order.title}</h2>
+                    <b className={styles.price}>{order.price} ₽</b>
+                </div>
             </div>
+            <FaTrash className={styles['del-button']} size={18} />
         </div>
     );
 }
